a4154bccd7025c820d41a07e51d5f17f5bc5c9ec
Oleksandr Yermolenko <oleksandr.yermolenko@gmail.com>
2011-04-24 10:36:10 +0300

    Reduce a number of neighbours processed inside rho() function

--- mdtk/potentials/manybody/TightBinding/TightBinding.cxx
+++ mdtk/potentials/manybody/TightBinding/TightBinding.cxx
@@ -256,6 +256,7 @@ TightBinding::rho(Atom &atom_i)
   {
     Atom& atom_j = *(NL(atom_i)[j]);
     if (/*atom_j.globalIndex > atom_i.globalIndex &&*/ isHandled(atom_j))
+    if (r_vec_module_no_touch(atom_i,atom_j) < R(1,atom_i,atom_j))
     {
       rhoij += g(atom_i,atom_j);
     }  
@@ -271,6 +272,7 @@ TightBinding::drho(Atom &atom_i, Atom &datom)
   {
     Atom& atom_j = *(NL(atom_i)[j]);
     if (/*atom_j.globalIndex > atom_i.globalIndex &&*/ isHandled(atom_j))
+    if (r_vec_module_no_touch(atom_i,atom_j) < R(1,atom_i,atom_j))
     {
       Derrho += dg(atom_i,atom_j,datom);
       TRACE(dg(atom_i,atom_j,datom));
