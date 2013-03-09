57f3d9c394a368a497509f8f3644c0cba86da870
Oleksandr Yermolenko <oleksandr.yermolenko@gmail.com>
2013-03-09 14:12:53 +0200

    Completely remove differentiation on cluster type

--- applications/mdbuilder/experiments/Clusters.cpp
+++ applications/mdbuilder/experiments/Clusters.cpp
@@ -159,11 +159,6 @@ optimize_single(SimLoop& simloop, gsl_rng* rng)
       {
         REQUIRE(mdloop.atoms.size() > 0);
         ElementID id = mdloop.atoms[0].ID;
-/*
-        if ((id == Cu_EL && mdloop.thermalBath.To > 1000.0*K) ||
-            (id == Au_EL && mdloop.thermalBath.To > 1500.0*K) ||
-            (id != Cu_EL && id != Au_EL))
-*/
         {
           snapshots.push_back(OptiSnapshot(mdloop.atoms,T));
           cerr << "Snapshot saved." << std::endl;
