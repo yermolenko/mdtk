f9c147d726f64ef2861a7f7f5daae9412c129500
Oleksandr Yermolenko <oleksandr.yermolenko@gmail.com>
2015-07-27 23:00:24 +0300

    Eliminate memory leaks in constructors of Ackland and FLJ

--- mdtk/potentials/manybody/Ackland/Ackland.hpp
+++ mdtk/potentials/manybody/Ackland/Ackland.hpp
@@ -3,8 +3,8 @@
    gold, silver and their alloys (header file).
    See [G.J. Ackland and V. Vitek, Phys. Rev. B 41, 10324 (1990)]
 
-   Copyright (C) 2007, 2008, 2009, 2012, 2013 Oleksandr Yermolenko
-   <oleksandr.yermolenko@gmail.com>
+   Copyright (C) 2007, 2008, 2009, 2012, 2013, 2015 Oleksandr
+   Yermolenko <oleksandr.yermolenko@gmail.com>
 
    This file is part of MDTK, the Molecular Dynamics Toolkit.
 
@@ -54,6 +54,7 @@ public:
   void setupPotential();
 
   Ackland();
+  virtual ~Ackland();
   Float getRcutoff() const {return max3(rk_[Au][Au][1],rk_[Ag][Ag][1],rk_[Cu][Cu][1]);} // needs unification!!!
 private:
   enum {ECOUNT = 3};
