f013c17376de1856139a366b78edf9c75a6d3dff
Oleksandr Yermolenko <oleksandr.yermolenko@gmail.com>
2015-11-05 14:22:19 +0200

    Fix a typo

--- configure-build-install-icc-heavy-optimize.sh
+++ configure-build-install-icc-heavy-optimize.sh
@@ -4,7 +4,7 @@
 # (Passing heavy optimize options to the Intel C++ compiler, see
 # top-level CMakeLists.txt for details).
 #
-# Copyright (C) 2009, 2011 Oleksandr Yermolenko
+# Copyright (C) 2009, 2011, 2015 Oleksandr Yermolenko
 # <oleksandr.yermolenko@gmail.com>
 #
 # Copying and distribution of this file, with or without modification,
@@ -17,7 +17,7 @@ cd _build-icc
 #  uncomment the next line when using icc 11.1 with gcc 4.5 or higher
 #  export GXX_INCLUDE=/usr/include/c++/4.4
   source /opt/intel/Compiler/11.1/064/bin/intel64/iccvars_intel64.sh
-  LANG=C CC=icc CXX=icpc cmake -D BUILD_INTELCC=1 -D MDTK_HEAVY_OPTIMZE=1 ..
+  LANG=C CC=icc CXX=icpc cmake -D BUILD_INTELCC=1 -D MDTK_HEAVY_OPTIMIZE=1 ..
   LANG=C make
   LANG=C make install
 cd ..
