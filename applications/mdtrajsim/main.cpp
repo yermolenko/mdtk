446cba75faafa7a844de5b696ea6ba24c6409999
Oleksandr Yermolenko <oleksandr.yermolenko@gmail.com>
2015-11-14 13:46:54 +0200

    Move auxiliary mdtrajsim functions to a separate module

--- applications/mdtrajsim/main.cpp
+++ applications/mdtrajsim/main.cpp
@@ -26,15 +26,6 @@
 #include <cstring>
 #include <algorithm>
 
-#ifdef __WIN32__
-#include <windows.h>
-#include <WinBase.h>
-#else
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/utsname.h>
-#endif
-
 #ifdef MPIBATCH
 #include "mpi.h"
 #endif
@@ -44,6 +35,8 @@
 #include <mdtk/SnapshotList.hpp>
 
 #include "../common.h"
+
+#include "tools.hpp"
 
 using namespace mdtk;
 
@@ -124,193 +117,6 @@ isAlreadyFinished()
       ifinal.close();
   }
   return false;
-}
-
-#ifdef MPIBATCH
-int
-comm_rank()
-{
-  int comm_rank;
-  MPI_TEST_SUCCESS(MPI_Comm_rank(MPI_COMM_WORLD,&comm_rank));
-  return comm_rank;
-}
-
-int
-comm_size()
-{
-  int comm_size;
-  MPI_TEST_SUCCESS(MPI_Comm_size(MPI_COMM_WORLD,&comm_size));
-  return comm_size;
-}
-
-std::string
-comm_name()
-{
-  char comm_name[MPI_MAX_PROCESSOR_NAME+1];
-  int len;
-  MPI_TEST_SUCCESS(MPI_Get_processor_name(comm_name,&len));
-  return comm_name;
-}
-#endif
-
-std::string
-getProcessID()
-{
-  std::ostringstream ossid;
-#ifdef MPIBATCH
-  ossid << "-" << comm_rank();
-#endif
-#ifdef __WIN32__
-  ossid << "-" << GetCurrentProcessId();
-#else
-  ossid << "-" << getpid();
-  struct utsname buffer;
-  if (uname(&buffer) == 0)
-    ossid << "-" << buffer.nodename;
-#endif
-  return ossid.str();
-}
-
-const std::string lockFilenameBase = "mdtrajsim.lock";
-std::string myLockFilename = lockFilenameBase;
-
-bool
-isLockedByOthers()
-{
-  std::vector<std::string> cwdfiles
-    = yaatk::listFiles(yaatk::getcwd());
-  for(size_t i = 0; i < cwdfiles.size(); ++i)
-  {
-    const std::string& cwdfile = cwdfiles[i];
-    if (cwdfile.find(lockFilenameBase) != std::string::npos &&
-        cwdfile != myLockFilename)
-      return true;
-  }
-
-  return false;
-}
-
-void
-placeMyLock()
-{
-  std::ifstream test(myLockFilename.c_str());
-  if (!test)
-  {
-    std::ofstream folock(myLockFilename.c_str());
-    folock.close();
-  }
-  else
-    throw Exception("Lockfile aready exists");
-}
-
-void
-removeMyLock()
-{
-  std::ifstream test(myLockFilename.c_str());
-  if (test)
-  {
-    int retval = yaatk::remove(myLockFilename);
-    if (retval)
-      throw Exception("Cannot remove lockfile");
-  }
-}
-
-void
-shrinkLogFiles()
-{
-  int retcode;
-
-  yaatk::DataState ds;
-
-  const std::string dElogfilename="dE.dat";
-  if (yaatk::exists(dElogfilename))
-    return;
-
-  const std::string logfilename="stdout.txt";
-  {
-    size_t numberOfLogfiles = 0;
-    std::vector<std::string> cwdfiles
-      = yaatk::listFiles(yaatk::getcwd());
-    for(size_t i = 0; i < cwdfiles.size(); ++i)
-    {
-      const std::string& cwdfile = cwdfiles[i];
-      if (cwdfile.find(logfilename) != std::string::npos)
-      {
-        ++numberOfLogfiles;
-        if (numberOfLogfiles > 1)
-          return;
-      }
-    }
-  }
-
-  yaatk::text_ofstream stdout_txt_filtered(logfilename + "-filtered");
-  REQUIRE(stdout_txt_filtered.isOpened());
-  {
-    yaatk::text_ofstream dE_dat(dElogfilename);
-    REQUIRE(dE_dat.isOpened());
-    {
-      std::ifstream stdout_txt(logfilename.c_str());
-      REQUIRE(stdout_txt);
-      std::vector<std::string> stdout_txt_lines(100);
-      size_t stdout_txt_lines_index = 0;
-      size_t stdout_txt_lines_read = 0;
-
-      char cline[1000];
-      while(stdout_txt.getline(cline, 1000-1, '\n'))
-      {
-        std::string line = cline;
-
-        stdout_txt_lines[stdout_txt_lines_index++] = line;
-        stdout_txt_lines_index %= stdout_txt_lines.size();
-
-        stdout_txt_lines_read++;
-        if (stdout_txt_lines_read <= stdout_txt_lines.size())
-          stdout_txt_filtered << line << "\n";
-
-        if (line.find("Eo+") != std::string::npos)
-        {
-          const std::string description = "dE/(Eo+Eb) : ";
-          if (line.find(description) == 0)
-            line.replace(0, description.size(), "");
-          dE_dat << line << "\n";
-        }
-      }
-      stdout_txt.close();
-
-      size_t tail_lines_count = stdout_txt_lines.size();
-      if (stdout_txt_lines_read <= stdout_txt_lines.size()*2)
-      {
-        if (tail_lines_count > stdout_txt_lines.size()*2 - stdout_txt_lines_read)
-          tail_lines_count -= stdout_txt_lines.size()*2 - stdout_txt_lines_read;
-        else
-          tail_lines_count = 0;
-      }
-      else
-      {
-        stdout_txt_filtered << "~~~~~~~~~~~~~~ cut ~~~~~~~~~~~~~~\n";
-      }
-
-      if (tail_lines_count > stdout_txt_lines_index)
-        stdout_txt_lines_index += stdout_txt_lines.size();
-      stdout_txt_lines_index -= tail_lines_count;
-
-      while (tail_lines_count > 0)
-      {
-        stdout_txt_filtered << stdout_txt_lines[stdout_txt_lines_index++] << "\n";
-        stdout_txt_lines_index %= stdout_txt_lines.size();
-
-        --tail_lines_count;
-      }
-    }
-    dE_dat.close();
-  }
-  stdout_txt_filtered.close();
-
-  retcode = yaatk::remove(logfilename);
-  REQUIRE(retcode == 0);
-  retcode = yaatk::rename(stdout_txt_filtered.getZippedFileName(),
-                          logfilename + stdout_txt_filtered.getZippedExt());
-  REQUIRE(retcode == 0);
 }
 
 int runTraj(std::string inputFilesId = "")
@@ -557,13 +363,9 @@ main(int argc, char *argv[])
 
   MPI_Barrier(MPI_COMM_WORLD);
 
-#ifdef __WIN32__
-  Sleep(10*comm_rank()*1000);
-#else
-  sleep(10*comm_rank());
+  SleepForSeconds(10*comm_rank());
 #endif
 
-#endif
   myLockFilename += getProcessID();
 
   int startFromImpact = 0;
