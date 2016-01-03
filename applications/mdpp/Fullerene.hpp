2ed31a04b9b498badfb240a8d41b56f24ffb6233
Oleksandr Yermolenko <oleksandr.yermolenko@gmail.com>
2016-01-03 14:52:45 +0200

    Add missing copyright notices

--- applications/mdpp/Fullerene.hpp
+++ applications/mdpp/Fullerene.hpp
@@ -1,3 +1,25 @@
+/*
+   The Fullerene class (header file).
+
+   Copyright (C) 2010, 2011, 2012 Oleksandr Yermolenko
+   <oleksandr.yermolenko@gmail.com>
+
+   This file is part of MDTK, the Molecular Dynamics Toolkit.
+
+   MDTK is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   MDTK is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with MDTK.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef mdpp_Fullerene_hpp
 #define mdpp_Fullerene_hpp
 
